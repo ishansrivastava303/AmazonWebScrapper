6655e19bebb19c94826a012638240b4c96abc682
ishansrivastava <ishansrivastava106@gmail.com>
2020-12-29 00:04:37 +0530

    added AmazonWebScraper

---
+++ amazonwebscraper.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+    //"fmt"
+    b "AmazonProject/writeJSON"
+    "log"
+    "net/http"
+    //"os"
+    "strings"
+    "github.com/PuerkitoBio/goquery"
+    "encoding/json"
+    //"database/sql"
+    //_ "github.com/mattn/go-sqlite3"
+    //_ "github.com/go-sql-driver/mysql"
+    //"io/ioutil"
+    //"github.com/gocolly/colly"
+)
+
+/*const(
+    sqlite3Str="sqlite3"
+    memStr=":memory:"
+)*/
+
+type Fact struct{
+    Name string `json:"name"`
+    ImageURL string `json:"imageURL"`
+    Description []string `json:"description"`
+    Price []string `json:"price"`
+    TotalReviews string `json:"totalReviews"`
+}
+
+
+type Product struct{
+    Url string `json:"Url"`
+    ProductDetails Fact `json:"ProductDetails"` 
+}
+
+var name,imageURL,totalReviews string
+var description [] string
+var price [] string
+
+func main() {
+    
+    url:="https://www.amazon.com/Nintendo-Switch-Gray-Joy%E2%80%91-HAC-001/dp/B07VJRZ62R/ref=pd_sim_nf_6?pd_rd_w=DuCMb&pf_rd_p=90765e8c-a52e-4c61-b338-0577ef37d819&pf_rd_r=K2K9FZBQZVVB2CGD6A96&pd_rd_r=63832bcf-fce5-407d-92d8-4aec92b7b470&pd_rd_wg=06OJt&pd_rd_i=B07VJRZ62R&psc=1"    
+    response, err := http.Get(url)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer response.Body.Close()
+
+    
+    document, err := goquery.NewDocumentFromReader(response.Body)
+    if err != nil {
+        log.Fatal("Error loading HTTP response body. ", err)
+    }
+
+    document.Find("#imgTagWrapperId").Each(func(index int, parent *goquery.Selection){
+    parent.Find("img").Each(func(index int, element *goquery.Selection) {
+        imgALT, existsALT := element.Attr("alt")
+        imgURL, existsURL := element.Attr("data-a-dynamic-image")
+        split:=strings.Split(imgURL,",")
+        indexOfJPG:=strings.Index(split[0],".jpg")
+        if existsALT && existsURL  {
+            name=imgALT
+            imageURL=split[0][2:(indexOfJPG+4)]
+        }
+    })})
+    
+    var reviewsFlag=1
+    document.Find("#acrCustomerReviewText").Each(func(index int, element *goquery.Selection) {
+        reviews:=element.Text()
+        if reviewsFlag==1{
+            totalReviews=reviews
+            reviewsFlag=0
+        }
+        
+    })
+    
+    priceFlag:=0
+    
+    document.Find("#priceblock_ourprice").Each(func(index int, element *goquery.Selection) {
+        priceFlag=1
+        //price:=element.Text()
+        price=append(price,element.Text())
+    })
+    
+    if priceFlag==0{
+      document.Find("#edition_0_price").Each(func(index int, element *goquery.Selection) {
+
+          str:=strings.ReplaceAll(element.Text(),"\n","")
+          price=append(price,strings.Trim(str," "))
+        
+    })
+        document.Find("#edition_1_price").Each(func(index int, element *goquery.Selection) {
+        
+       str:=strings.ReplaceAll(element.Text(),"\n","")        
+            price=append(price,strings.Trim(str," "))
+    }) 
+        //fmt.Println(prices)
+    }
+    
+      document.Find("#feature-bullets").Each(func(index int, feature *goquery.Selection) {
+      
+          feature.Find("li").Each(func(index int, list *goquery.Selection) {
+              str:=strings.ReplaceAll(list.Text(),"\n","")
+              description=append(description,strings.Trim(str," "))
+    })    
+    })
+    
+    
+    fact:=Fact{
+        Name:name,
+        ImageURL:imageURL,
+        Description:description,
+        Price:price,
+        TotalReviews:totalReviews,
+    }
+    
+    
+    allFacts:=make([]Fact,0)
+    allFacts=append(allFacts,fact)
+    
+    
+    
+    product:=Product{
+        Url:url,
+        ProductDetails:fact,
+    }
+    productDetails:=make([]Product,0)
+    productDetails=append(productDetails,product)
+    //enc:=json.NewEncoder(os.Stdout)
+    //enc.SetIndent(""," ")
+    //enc.Encode(allFacts)
+    s,_:=json.MarshalIndent(product,""," ")
+    //log.Println(string(b))
+    b.DbStore(string(s),fact.Name)
+    
+    
+    
+}
+
+
+    
+
---
+++ go.mod
@@ -0,0 +1,9 @@
+module AmazonProject
+
+go 1.15
+
+require (
+	github.com/PuerkitoBio/goquery v1.6.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.6
+)
